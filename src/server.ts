7760400b2510e7f27fb090e9815787a5b1a95444
Rodrigo Costa <rodrigocosta47@outlook.com>
2023-01-18 18:01:48 -0300

    Configurei o prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-console.log('Olá mundo');
+import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get('/', async (req) => {
+    const habitos =  await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    });
+
+    return habitos;
+});
+
+app.listen({
+    port:3333,
+}).then(()=>{
+    console.log("HTTP serve runing");
+});
