e18eea0381555715305392924acd56ef3ec63ef0
Rodrigo Costa <rodrigocosta47@outlook.com>
2023-01-18 21:11:20 -0300

    finalizei as configuracoes base do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import {PrismaClient} from '@prisma/client';
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors);
+
 app.get('/', async (req) => {
-    const habitos =  await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    });
+    const habitos =  await prisma.habit.findMany();
 
     return habitos;
 });
